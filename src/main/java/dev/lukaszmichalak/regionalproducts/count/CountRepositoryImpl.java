cbfcfd7ed70e02ca3d62732096dc43735579e2c0
Łukasz Michalak <lukaszmichalak0402@gmail.com>
2024-12-30 15:03:01 +0100

    SQL refactor

--- src/main/java/dev/lukaszmichalak/regionalproducts/count/CountRepositoryImpl.java
+++ src/main/java/dev/lukaszmichalak/regionalproducts/count/CountRepositoryImpl.java
@@ -15,9 +15,9 @@ class CountRepositoryImpl implements CountRepository {
   private static final String countsSql =
       """
       SELECT v.name AS voivodeship_name, COUNT(p.id) AS product_count
-      FROM voivodeship v
-      LEFT JOIN product p ON v.id = p.voivodeship_id
-      GROUP BY voivodeship_name;
+      FROM voivodeship AS v
+      LEFT JOIN product AS p ON v.id = p.voivodeship_id
+      GROUP BY v.name;
       """;
 
   @Override
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -7,13 +7,13 @@ CREATE TABLE IF NOT EXISTS voivodeship
     description_en        TEXT NOT NULL,
     description_pl        TEXT NOT NULL,
     coat_of_arms_filename TEXT NOT NULL,
-    CHECK (trim(creation_date) != ''),
-    CHECK (trim(name) != ''),
-    CHECK (length(code) == 2),
-    CHECK (trim(description_en) != ''),
-    CHECK (trim(description_pl) != ''),
+    CHECK (TRIM(creation_date) != ''),
+    CHECK (TRIM(name) != ''),
+    CHECK (LENGTH(code) == 2),
+    CHECK (TRIM(description_en) != ''),
+    CHECK (TRIM(description_pl) != ''),
     CHECK (description_en != description_pl),
-    CHECK (trim(coat_of_arms_filename) != ''),
+    CHECK (TRIM(coat_of_arms_filename) != ''),
     CHECK (coat_of_arms_filename LIKE '%.png')
 ) strict;
 
@@ -22,8 +22,8 @@ CREATE TABLE IF NOT EXISTS product_type
     id            INTEGER PRIMARY KEY AUTOINCREMENT,
     creation_date TEXT DEFAULT CURRENT_TIMESTAMP,
     name          TEXT NOT NULL UNIQUE,
-    CHECK (trim(creation_date) != ''),
-    CHECK (trim(name) != '')
+    CHECK (TRIM(creation_date) != ''),
+    CHECK (TRIM(name) != '')
 ) strict;
 
 CREATE TABLE IF NOT EXISTS product
@@ -34,12 +34,12 @@ CREATE TABLE IF NOT EXISTS product
     voivodeship_id  INTEGER NOT NULL,
     date_of_entry   TEXT    NOT NULL,
     creation_date   TEXT DEFAULT CURRENT_TIMESTAMP,
-    CHECK (trim(name) != ''),
+    CHECK (TRIM(name) != ''),
     FOREIGN KEY (product_type_id) REFERENCES product_type (id),
     FOREIGN KEY (voivodeship_id) REFERENCES voivodeship (id),
     UNIQUE(name, product_type_id, voivodeship_id),
-    CHECK (trim(date_of_entry) != ''),
-    CHECK (trim(creation_date) != ''),
+    CHECK (TRIM(date_of_entry) != ''),
+    CHECK (TRIM(creation_date) != ''),
     CHECK (date_of_entry <= creation_date),
     CHECK (date_of_entry >= '1900-01-01')
 ) strict;
