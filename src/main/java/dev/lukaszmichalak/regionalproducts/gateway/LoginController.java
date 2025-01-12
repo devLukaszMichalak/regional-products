8569a1800f0ad83f78a3fbe943920f5d8f5fefbd
Łukasz Michalak <lukaszmichalak0402@gmail.com>
2025-01-12 20:32:14 +0100

    login in

--- src/main/java/dev/lukaszmichalak/regionalproducts/gateway/LoginController.java
+++ src/main/java/dev/lukaszmichalak/regionalproducts/gateway/LoginController.java
@@ -1,19 +1,37 @@
 package dev.lukaszmichalak.regionalproducts.gateway;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Controller()
+@RequiredArgsConstructor
 @RequestMapping("/{lang}/login")
 class LoginController {
 
+  private final AuthenticationManager authenticationManager;
+
   @GetMapping()
-  public String getLogin(
-      Model model, @RequestParam(value = "hadError", required = false) boolean hadError) {
+  public String getLogin(Model model, @RequestParam(required = false) boolean hadError) {
     model.addAttribute("hadError", hadError);
     return "login";
+  }
+
+  @PostMapping()
+  public String login(
+      @PathVariable String lang, @RequestParam String username, @RequestParam String password) {
+
+    var auth = new UsernamePasswordAuthenticationToken(username, password);
+    try {
+      authenticationManager.authenticate(auth);
+    } catch (AuthenticationException e) {
+      return "redirect:/" + lang + "/login?hadError=true";
+    }
+
+    return "redirect:/" + lang + "/voivodeships";
   }
 }
--- src/main/java/dev/lukaszmichalak/regionalproducts/security/SecurityConfig.java
+++ src/main/java/dev/lukaszmichalak/regionalproducts/security/SecurityConfig.java
@@ -3,6 +3,8 @@ package dev.lukaszmichalak.regionalproducts.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -37,14 +39,15 @@ public class SecurityConfig {
                     .requestMatchers(HttpMethod.POST, "/rank")
                     .authenticated()
                     .anyRequest()
-                    .permitAll())
-        .formLogin(fromLogin -> fromLogin.loginPage("/login"));
-//        .logout(
-            //            logout -> logout.logoutUrl("/perform_logout").deleteCookies("JSESSIONID")
-            //                    .logoutSuccessHandler(logoutSuccessHandler())
-//            );
+                    .permitAll());
 
     return http.build();
+  }
+
+  @Bean
+  public AuthenticationManager authenticationManager(
+      AuthenticationConfiguration authenticationConfiguration) throws Exception {
+    return authenticationConfiguration.getAuthenticationManager();
   }
 
   @Bean
