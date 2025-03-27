3582284f845903226bef36146317b950c76f1c83
Łukasz Michalak <lukaszmichalak0402@gmail.com>
2025-03-27 18:19:15 +0100

    Fixed n+1 problem for productType

--- src/main/java/dev/lukaszmichalak/regionalproducts/product/ProductRepository.java
+++ src/main/java/dev/lukaszmichalak/regionalproducts/product/ProductRepository.java
@@ -2,12 +2,26 @@ package dev.lukaszmichalak.regionalproducts.product;
 
 import java.util.List;
 import java.util.Optional;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.repository.ListCrudRepository;
+import org.springframework.lang.NonNull;
 
 interface ProductRepository extends ListCrudRepository<Product, Long> {
 
+  @NonNull
+  @Override
+  @EntityGraph(attributePaths = {"productType"})
+  Optional<Product> findById(@NonNull Long id);
+
+  @NonNull
+  @Override
+  @EntityGraph(attributePaths = {"productType"})
+  List<Product> findAll();
+
+  @EntityGraph(attributePaths = {"productType"})
   Optional<Product> findByName(String name);
 
+  @EntityGraph(attributePaths = {"productType"})
   List<Product> findByVoivodeshipId(Long name);
 
   long countByVoivodeshipId(Long id);
